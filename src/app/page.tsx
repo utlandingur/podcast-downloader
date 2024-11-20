4dd9b0682204f10600650256f94fc1869a07cf1b
sessiher <sesarh@icloud.com>
2024-11-20 21:34:12 +0000

    import fonts in inital rather then rootLayour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { PodcastSearchBar } from "@/components/PodcastSearchBar";
+import { geistSans, geistMono } from "./fonts";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 justify-center items-center sm:items-start">
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main
+        className={`flex flex-col gap-8 justify-center items-center sm:items-start ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <h1>Download podcasts to mp3</h1>
         <PodcastSearchBar />
       </main>
