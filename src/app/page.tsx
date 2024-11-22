fc73fc24b31de8556122af7e21f913f7a08f424c
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-11-22 11:35:19 +0000

    try force cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { PodcastSearchBar } from "@/components/google";
+import { PodcastSearchBar } from "@/components/podcastSearchBar";
 
 import { geistSans, geistMono } from "./fonts";
 import { Suspense } from "react";
