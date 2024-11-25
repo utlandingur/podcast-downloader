3b1ec1a1121e69b776cb2919944b4e1db9d10dc7
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-11-25 16:08:00 +0000

    add link to how it works

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className={cn("p-4 font-semibold bg-primary")}>
+    <div
+      className={cn("p-4 flex w-full font-semibold bg-primary justify-between")}
+    >
       <Link href={"/"} className="text-primary-foreground">
         PodcastToMp3.com
+      </Link>
+      <Link href={"/how-it-works"} className="text-primary-foreground">
+        How it works
       </Link>
     </div>
   );
