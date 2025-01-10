fec0783852362d3e996a521ad30d1b39021838d7
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-01-10 13:13:19 +0000

    remove unneeded session callback

--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-import { getUser } from "@/serverActions/userActions";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
@@ -9,17 +8,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     // maxAge: is 30 days by default
   },
   callbacks: {
-    async session({ session }) {
-      const user = await getUser(session.user.email);
-      //add user to session
-      if (!user) {
-        return session;
-      }
-
-      session.user = user.toObject();
-
-      return session;
-    },
     async signIn() {
       return true;
     },
