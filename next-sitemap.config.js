a856066471f8153a3a0d395907c2bef048e62245
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-02-14 10:28:09 +0000

    create sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://podcasttomp3.com', // Replace with your actual domain
+  generateRobotsTxt: true, // Automatically generates `robots.txt`
+};
